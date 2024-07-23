3bc8a182b2bd1e77cff0761f695910563818a78b
Yozachar <38415384+yozachar@users.noreply.github.com>
2024-07-23 16:18:03 +0530

    feat: adds csv to sql, need optimization
    - adds `dask` dependency

---
+++ src/cajon/backend/transactions.py
@@ -0,0 +1,35 @@
+"""Transactions."""
+
+# standard
+from os import environ
+from pathlib import Path
+
+# external
+from dask.dataframe import read_csv  # pyright: ignore[reportPrivateImportUsage]
+from dotenv import load_dotenv
+
+# local
+from . import cajon
+
+load_dotenv()
+
+
+def csv_to_sql(source: Path):
+    """CSV to SQL."""
+    # TODO: optimize
+    _pg_user = environ["POSTGRES_USER"]
+    _pg_passwd = environ["POSTGRES_PASSWORD"]
+    _db_host = environ["PG_DB_HOST"]
+    _db_port = environ["PG_DB_PORT"]
+    _db_name = environ["POSTGRES_DB"]
+    uri = f"postgresql+psycopg://{_pg_user}:{_pg_passwd}@{_db_host}:{_db_port}/{_db_name}"
+
+    t_name = source.name.split(" ")[0]
+    ddf = read_csv(source)
+    ddf.to_sql(t_name, uri, if_exists="replace", method="multi")
+
+
+def action():
+    """Action."""
+    source = cajon / "backend/uploads/product_data 2024-07-23 16:30:01.590859.csv"
+    csv_to_sql(source)
